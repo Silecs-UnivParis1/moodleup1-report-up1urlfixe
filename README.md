265d46b4392f42d8d438d566ca8f62f94f8e9e9f
Guillaume Allegre <guillaume.allegre@silecs.info>
2020-10-16 23:32:06 +0200

    migrate custom_info_data to customfield_data

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 `report_up1urlfixe`
 
+Ce rapport complète le plugin `local/redirection` qui effectue le travail principal.
+
 (C) 2013-2020  Silecs
--- locallib.php
+++ locallib.php
@@ -20,18 +20,18 @@ require_once(dirname(dirname(__DIR__)).'/config.php'); // global moodle config f
 function report_up1urlfixe_liste() {
     global $DB, $CFG;
     $res = array();
-    $sql = "SELECT c.shortname AS crsname, cid.objectid AS crsid, cid.data "
-          . "FROM custom_info_data cid "
-          . "JOIN custom_info_field cif ON (cid.fieldid = cif.id) "
-          . "JOIN course c ON (c.id = cid.objectid AND cid.objectname = :obj) "
-          . "WHERE cif.shortname = :field AND cid.data != ''  ORDER BY cid.data ";
-    $records = $DB->get_records_sql($sql, ['obj' => 'course', 'field' => 'up1urlfixe']);
+    $sql = "SELECT c.shortname AS crsname, cd.instanceid AS crsid, cd.charvalue "
+          . "FROM {customfield_data} cd "
+          . "JOIN {customfield_field} cf ON (cd.fieldid = cf.id) "
+          . "JOIN {course} c ON (c.id = cd.instanceid) "
+          . "WHERE cf.shortname = 'up1urlfixe' AND cd.charvalue != ''  ORDER BY cd.charvalue ";
+    $records = $DB->get_records_sql($sql);
 
     foreach ($records as $record) {
-        $urlredirection = $CFG->wwwroot . '/fixe/' . $record->data;
-        $urlcourse = new moodle_url('course/view.php', ['id' => $record->crsid]);
+        $urlredirection = $CFG->wwwroot . '/fixe/' . $record->charvalue;
+        $urlcourse = new moodle_url('/course/view.php', ['id' => $record->crsid]);
         $res[] = array (
-            $record->data,
+            $record->charvalue,
             html_writer::link($urlredirection, $urlredirection),
             html_writer::link($urlcourse, $record->crsname),
         );
@@ -47,18 +47,18 @@ function report_up1urlfixe_liste() {
 function report_up1urlfixe_doublons() {
     global $DB;
     $res = array();
-    $sql = "SELECT COUNT(DISTINCT cid.id) AS cnt, GROUP_CONCAT(c.shortname) AS names, GROUP_CONCAT(cid.objectid) AS crsid, cid.data "
-          . "FROM custom_info_data cid "
-          . "JOIN custom_info_field cif ON (cid.fieldid = cif.id) "
-          . "JOIN course c ON (c.id = cid.objectid AND cid.objectname = :obj) "
-          . "WHERE cif.shortname = :field "
-          . "GROUP BY cid.data HAVING cnt > 1";
-    $doublons = $DB->get_records_sql($sql, ['obj' => 'course', 'field' => 'up1urlfixe']);
+    $sql = "SELECT COUNT(DISTINCT cd.id) AS cnt, GROUP_CONCAT(c.shortname) AS names, GROUP_CONCAT(cd.instanceid) AS crsid, cd.charvalue "
+          . "FROM {customfield_data} cd "
+          . "JOIN {customfield_field} cf ON (cd.fieldid = cf.id) "
+          . "JOIN {course} c ON (c.id = cd.instanceid) "
+          . "WHERE cf.shortname = 'up1urlfixe' AND cd.charvalue != '' "
+          . "GROUP BY cd.charvalue HAVING cnt > 1";
+    $doublons = $DB->get_records_sql($sql);
 
     foreach ($doublons as $doublon) {
         $res[] = array (
             $doublon->cnt,
-            $doublon->data,
+            $doublon->charvalue,
             $doublon->names,
             $doublon->crsid,
         );
@@ -75,16 +75,16 @@ function report_up1urlfixe_doublons() {
 function report_up1urlfixe_supprimes() {
     global $DB;
     $res = array();
-    $sql = "SELECT c.shortname AS name, cid.objectid AS crsid, cid.data "
-          . "FROM custom_info_data cid "
-          . "JOIN custom_info_field cif ON (cid.fieldid = cif.id) "
-          . "LEFT JOIN course c ON (c.id = cid.objectid AND cid.objectname = :obj) "
-          . "WHERE cif.shortname = :field AND c.id IS NULL";
-    $supprimes = $DB->get_records_sql($sql, ['obj' => 'course', 'field' => 'up1urlfixe']);
+    $sql = "SELECT c.shortname AS name, cd.instanceid AS crsid, cd.charvalue "
+          . "FROM {customfield_data} cd "
+          . "JOIN {customfield_field} cf ON (cd.fieldid = cf.id) "
+          . "LEFT JOIN {course} c ON (c.id = cd.instanceid) "
+          . "WHERE cf.shortname = 'up1urlfixe' AND c.id IS NULL";
+    $supprimes = $DB->get_records_sql($sql);
 
     foreach ($supprimes as $supprime) {
         $res[] = array (            
-            $supprime->data,
+            $supprime->charvalue,
             $supprime->name,
             $supprime->crsid,
         );
