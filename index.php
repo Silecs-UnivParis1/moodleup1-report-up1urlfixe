8e894f74d294456fbb3b67b1f7a3853c7657d7a4
Guillaume Allegre <guillaume.allegre@silecs.info>
2020-10-21 17:36:14 +0200

    adaptation à Moodle 3.9

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 /**
  * Version info
  *
- * @package    report
- * @subpackage up1urlfixe
- * @copyright  2012-2016 Silecs {@link http://www.silecs.info/societe}
+ * @package    report_up1urlfixe
+ * @copyright  2016-2020 Silecs {@link http://www.silecs.info/societe}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
--- version.php
+++ version.php
@@ -3,14 +3,13 @@
 /**
  * Version info
  *
- * @package    report
- * @subpackage up1urlfixe
- * @copyright  2012-2016 Silecs {@link http://www.silecs.info/societe}
+ * @package    report_up1urlfixe
+ * @copyright  2016-2020 Silecs {@link http://www.silecs.info/societe}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016051201;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2012061700;        // Requires this Moodle version
+$plugin->version   = 2020102100;       // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2020061502;       // Requires this Moodle version
 $plugin->component = 'report_up1urlfixe'; // Full name of the plugin (used for diagnostics)
